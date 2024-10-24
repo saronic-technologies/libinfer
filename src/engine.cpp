a17c568d9bc9b658c4a92fdb5a39a81803d95029
Joseph Fox-Rabinovitz <joseph.fox-rabinovitz@saronic.com>
2024-10-23 20:44:58 -0500

    feat: make output folder if not exists

--- src/engine.cpp
+++ src/engine.cpp
@@ -234,7 +234,10 @@ void Engine::build() {
     throw std::runtime_error("Could not create network serializer");
   }
 
-  // Write the engine to disk
+  // Write the engine to disk (create output folder if necessary)
+  if (std::filesystem::create_directories(secondaryPath)) {
+    spdlog::info("Created output folder for engines at {}", secondaryPath);
+  }
   std::ofstream outfile(mEnginePath, std::ofstream::binary);
   if (!outfile.is_open()) {
     throw std::runtime_error("Could not open output file");
